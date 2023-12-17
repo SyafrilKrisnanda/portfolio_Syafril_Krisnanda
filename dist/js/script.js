0c4bf94e6bc421eca7e897bb4f2d15d48cc15d3f
SyafrilKrisnanda <syafril.krisnanda.sk@gmail.com>
2023-12-17 18:01:11 +0700

    nambah portfolio page

--- dist/js/script.js
+++ dist/js/script.js
@@ -19,3 +19,46 @@ hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("hamburger-active");
   navMenu.classList.toggle("hidden");
 });
+
+// animated
+document.addEventListener("DOMContentLoaded", function () {
+  // Scroll smooth untuk tautan di navbar
+  var links = document.querySelectorAll("nav a");
+
+  links.forEach(function (link) {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      var targetId = this.getAttribute("href").substring(1);
+      var targetElement = document.getElementById(targetId);
+
+      if (targetElement) {
+        scrollTo(targetElement);
+      }
+    });
+  });
+
+  // Scroll smooth untuk tautan di site map (footer)
+  var siteMapLinks = document.querySelectorAll("footer a");
+
+  siteMapLinks.forEach(function (link) {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      var targetId = this.getAttribute("href").substring(1);
+      var targetElement = document.getElementById(targetId);
+
+      if (targetElement) {
+        scrollTo(targetElement);
+      }
+    });
+  });
+
+  function scrollTo(element) {
+    window.scroll({
+      behavior: "smooth",
+      left: 0,
+      top: element.offsetTop,
+    });
+  }
+});
